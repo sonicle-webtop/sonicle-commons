19c9751c5ca19a55aa2d169e18981620e2cfca23
Matteo Albinola <malbinola@sonicle.com>
2017-03-02 10:52:31 +0100

    Update pom groupId

---
+++ src/main/java/com/sonicle/commons/db/PostgresUtils.java
@@ -0,0 +1,56 @@
+/* 
+ * Copyright (C) 2014 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle[dot]com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Copyright (C) 2014 Sonicle S.r.l.".
+ */
+package com.sonicle.commons.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+//import org.postgresql.largeobject.LargeObject;
+//import org.postgresql.largeobject.LargeObjectManager;
+
+/**
+ *
+ * @author malbinola
+ */
+public class PostgresUtils {
+	/*
+	public static byte[] readLargeObject(Connection con, long oid) throws SQLException {
+		LargeObjectManager lom = (con.unwrap(org.postgresql.PGConnection.class)).getLargeObjectAPI();
+		LargeObject lo = lom.open(oid, LargeObjectManager.READ);
+		
+		byte bytes[] = new byte[lo.size()];
+		lo.read(bytes, 0, lo.size());
+		lo.close();
+		return bytes;
+	}
+	*/
+}
