16390080e145b5d01cf5b80340449feef27bd30e
Gabriele Bulfon <gbulfon@sonicle.com>
2017-05-15 18:05:07 +0200

    add getText to obtain primary text content

--- src/main/java/com/sonicle/commons/MailUtils.java
+++ src/main/java/com/sonicle/commons/MailUtils.java
@@ -36,6 +36,9 @@ import java.io.*;
 import java.util.*;
 import java.io.UnsupportedEncodingException;
 import java.util.regex.Pattern;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import net.htmlparser.jericho.Renderer;
@@ -779,5 +782,45 @@ public class MailUtils {
   }
 
 
-} 
+	/**
+     * Return the primary text content of the message.
+     */
+    public static String getText(Part p) throws
+                MessagingException, IOException {
+        if (p.isMimeType("text/*")) {
+            String s = (String)p.getContent();
+            if (p.isMimeType("text/html")) s=htmlToText(s);
+            return s;
+        }
+
+        if (p.isMimeType("multipart/alternative")) {
+            // prefer html text over plain text
+            Multipart mp = (Multipart)p.getContent();
+            String text = null;
+            for (int i = 0; i < mp.getCount(); i++) {
+                Part bp = mp.getBodyPart(i);
+                if (bp.isMimeType("text/plain")) {
+                    if (text == null)
+                        text = getText(bp);
+                    continue;
+                } else if (bp.isMimeType("text/html")) {
+                    String s = getText(bp);
+                    if (s != null)
+                        return s;
+                } else {
+                    return getText(bp);
+                }
+            }
+            return text;
+        } else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart)p.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                String s = getText(mp.getBodyPart(i));
+                if (s != null)
+                    return s;
+            }
+        }
+
+        return null;
+    }} 
 
