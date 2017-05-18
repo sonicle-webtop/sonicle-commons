857650aef0ced7fe87765c8e25bbd158f37a3514
Matteo Albinola <malbinola@sonicle.com>
2017-05-18 14:36:27 +0200

    Add new forValue impl. in EnumUtils

--- src/main/java/com/sonicle/commons/EnumUtils.java
+++ src/main/java/com/sonicle/commons/EnumUtils.java
@@ -32,13 +32,20 @@
  */
 package com.sonicle.commons;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.util.EnumSet;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
  * @author malbinola
  */
 public class EnumUtils {
+	
+	public static final Gson GSON = new GsonBuilder()
+		.serializeNulls()
+		.create();
 	
 	public static <E extends Enum<E>> E getEnum(Class<E> enumClass, String enumName) {
 		if(enumName == null) return null;
@@ -58,15 +65,71 @@ public class EnumUtils {
 		return en1.equals(en2);
 	}
 	
-	public static <E extends Enum<E>> E forValue(Class<E> enumClass, String enumValue) {
-		if (enumValue == null) return null;
+	@Deprecated
+	public static <E extends Enum<E>> E forValue(Class<E> enumClass, String value) {
+		return forValue(value, enumClass);
+	}
+	
+	/**
+	 * Convenience method for getting the enum from its string value representation.
+	 * The enum's toString() method will be used for getting the value, so if you
+	 * need to change the default return you need to override properly toString() 
+	 * method in the enum class.
+	 * 
+	 * Example:
+	 * 
+	 * public enum MyEnum {
+	 *		VAL_1("val1"), VAL_2("val2");
+	 *		private final String value;
+	 *		private MyEnum(String value) { this.value = value; }
+	 *		@Override
+	 *		public String toString() { return value; }
+	 * }
+	 * 
+	 * @param <E>
+	 * @param value The String value
+	 * @param enumClass The enum class
+	 * @return The matching enum if found, null otherwise
+	 */
+	public static <E extends Enum<E>> E forValue(String value, Class<E> enumClass) {
+		if (value == null) return null;
 		for (E e : EnumSet.allOf(enumClass)) {
-			if (enumValue.equals(e.toString())) return e;
+			if (value.equals(e.toString())) return e;
 		}
 		return null;
 	}
 	
 	public static <E extends Enum<E>> String getValue(E en) {
 		return (en == null) ? null : en.toString();
+	}
+	
+	/**
+	 * Convenience method for getting the enum from its string value representation.
+	 * JSON library annotation will be used for getting the serializedName, so you
+	 * need to annotate the enum class properly before using this method.
+	 * 
+	 * Example:
+	 * 
+	 * public enum MyEnum {
+	 *		@SerializedName("val1") VAL_1, @SerializedName("val2") VAL_2;
+	 * }
+	 * 
+	 * @param <E>
+	 * @param serializedName The serialized name
+	 * @param enumClass The enum class
+	 * @return The matching enum if found, null otherwise
+	 */
+	public static <E extends Enum<E>> E forSerializedName(String serializedName, Class<E> enumClass) {
+		return (serializedName == null) ? null : GSON.fromJson(serializedName, enumClass);
+	}
+	
+	public static <E extends Enum<E>> String toSerializedName(E en) {
+		if (en == null) {
+			return null;
+		} else {
+			final String s = GSON.toJson(en, en.getClass());
+			return StringUtils.substring(s, 1, s.length()-1);
+		}
+		//return (en == null) ? null : GSON.toJson(en, en.getClass());
 	}
 }
