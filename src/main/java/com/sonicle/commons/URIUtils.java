dfcecfd2504e36cd8d1bb975bf17eb5129031a76
Matteo Albinola <malbinola@sonicle.com>
2017-08-11 12:24:49 +0200

    Add some new utility methods

--- src/main/java/com/sonicle/commons/URIUtils.java
+++ src/main/java/com/sonicle/commons/URIUtils.java
@@ -37,6 +37,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
 
 /**
  *
@@ -65,9 +66,51 @@ public class URIUtils {
 		return new URI(uri).getScheme();
 	}
 	
-	public static String[] parseUserInfo(URI uri) {
+	public static String[] getUserInfo(URI uri) {
 		String[] tokens = StringUtils.split(uri.getUserInfo(), ":", 2);
 		if(tokens == null || tokens.length == 0) return null;
 		return (tokens.length == 1) ? new String[]{tokens[0], null} : tokens;
+	}
+	
+	public static URI appendPath(URI uri, String path) throws URISyntaxException {
+		URIBuilder ub = new URIBuilder(uri);
+		appendPath(ub, path);
+		return ub.build();
+	}
+	
+	public static void appendPath(URIBuilder builder, String path) {
+		if (!StringUtils.isBlank(path)) {
+			final String p1 = StringUtils.defaultString(StringUtils.removeEnd(builder.getPath(), "/"));
+			final String p2 = StringUtils.defaultString(StringUtils.removeStart(path, "/"));
+			builder.setPath(p1 + "/" + p2);
+		}
+	}
+	
+	public static String asUserInfo(String username, String password) {
+		String userInfo = null;
+		if (!StringUtils.isBlank(username)) {
+			userInfo = username;
+			if (!StringUtils.isBlank(password)) {
+				userInfo += ":";
+				userInfo += password;
+			}
+		}
+		return userInfo;
+	}
+	
+	public static String[] toURIStrings(URI[] uris, boolean ascii) {
+		String[] ss = new String[uris.length];
+		for(int i=0; i<uris.length; i++) {
+			ss[i] = (ascii) ? toASCIIString(uris[i]) : toString(uris[i]);
+		}
+		return ss;
+	}
+	
+	public static String toASCIIString(URI uri) {
+		return (uri == null) ? null : uri.toASCIIString();
+	}
+	
+	public static String toString(URI uri) {
+		return (uri == null) ? null : uri.toString();
 	}
 }
