beac4833273aec497881c4b64e49a25acd5de39e
Matteo Albinola <malbinola@sonicle.com>
2018-08-08 13:01:56 +0200

    Add priority lock

---
+++ src/main/java/com/sonicle/commons/concurrent/ReentrantPriorityLock.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle[dot]com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Copyright (C) 2018 Sonicle S.r.l.".
+ */
+package com.sonicle.commons.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author malbinola
+ */
+public class ReentrantPriorityLock {
+	private final ReentrantLock lock1;
+	private final ReentrantLock lock2;
+	
+	public ReentrantPriorityLock() {
+		this.lock1 = new ReentrantLock(true);
+		this.lock2 = new ReentrantLock(true);
+	}
+	
+	public void lock() {
+		lock(Priority.LOW);
+	}
+	
+	public void lock(final Priority priority) {
+		switch (priority) {
+			case LOW:
+				lock1.lock();
+			case HIGH:
+				lock2.lock();
+		}
+	}
+	
+	public void unlock() {
+		lock2.unlock();
+		if (lock1.isHeldByCurrentThread()) {
+			lock1.unlock();
+		}
+	}
+	
+	public enum Priority {
+		HIGH, LOW
+	}
+}
